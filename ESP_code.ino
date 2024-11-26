679950f4bd12f3e05b8c2fae971dedfaf4e93ce5
GOD_shinigami <19430@mac.du.ac.in>
2024-11-26 13:16:05 +0530

    Removed unnecessary text

--- ESP_code.ino
+++ ESP_code.ino
@@ -1,8 +1,5 @@
-#include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <time.h>
-#include <sys/time.h>
 #include "adc.h"
 #include "mpu.h"
 
@@ -10,34 +7,17 @@ Adafruit_ADS1115 ads1;
 Adafruit_ADS1115 ads2;
 Adafruit_MPU6050 mpu;
 
-const char* ssid = "FMG_SERVER";
-const char* password = "AIIMS_FMG_SERVER";
+const char* ssid = "NESUG-4_2G";
+const char* password = "9911773437";
 const char* serverName = "http://192.168.1.37:5000/sensor";
 
+int counter = 0;
+
 void setup(void) {
-  Serial.begin(9600);
-  
+  Serial.begin(512000);
+    
   // WiFi
   WiFi.begin(ssid, password);
-  unsigned long startAttemptTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime <10000) {
-    delay(500);
-    Serial.println("Connecting...");
-  }
-
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Failed to connect to WiFi");
-  }
-
-  configTime(0, 0, "pool.ntp.org");
-  Serial.println("Waiting for time sync...");
-  while(!time(nullptr)) {
-    delay(500);
-    Serial.println(".");
-  }
-  Serial.println("\nTime synced");
 
   // ADC
   setupADC();
@@ -46,41 +26,21 @@ void setup(void) {
   setupMPU();
 }
 
-String timeWithMillisec() {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  struct tm* p_tm = gmtime(&tv.tv_sec);
-  char timestamp[30];
-  snprintf(timestamp, sizeof(timestamp), "%04d-%02d-%02d %02d:%02d:%02d.%03d",
-           p_tm->tm_year + 1900, p_tm->tm_mon + 1, p_tm->tm_mday,
-           p_tm->tm_hour, p_tm->tm_min, p_tm->tm_sec, (int)(tv.tv_usec / 1000));
-  return String(timestamp);
-}
-
 void loop(void) {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
-    http.begin(serverName);
-    http.addHeader("Content-Type", "application/json");
-
-    // Sensor read time
-    String read_timestamp = timeWithMillisec();
-    // Data send time
-    String send_timestamp = timeWithMillisec();
 
     float adcValues[8];
-    for (int i = 0; i < 4; i++)
-    {
+    for (int i = 0; i < 4; i++) {
       adcValues[i] = readADC(ads1, i); 
-      adcValues[i + 4] = readADC(ads2, i); 
+      adcValues[i + 4] = readADC(ads2, i);
     }
 
     sensors_event_t a, g, temp;
     readMPU(a, g, temp);
 
-    String jsonData = "{\"sensor_read_time\": \"" + read_timestamp + 
-                      "\", \"data_send_time\": \""+ send_timestamp + 
-                      "\", \"AIN0\": " + String(adcValues[0], 3) + 
+    String jsonData = "{ \"counter\": " + String(counter) + // Add counter to JSON data
+                      ", \"AIN0\": " + String(adcValues[0], 3) + 
                       ", \"AIN1\": " + String(adcValues[1], 3) + 
                       ", \"AIN2\": " + String(adcValues[2], 3) + 
                       ", \"AIN3\": " + String(adcValues[3], 3) + 
@@ -94,17 +54,26 @@ void loop(void) {
                       ", \"Rotation_x\": " + String(g.gyro.x) + 
                       ", \"Rotation_y\": " + String(g.gyro.y) + 
                       ", \"Rotation_z\": " + String(g.gyro.z) + "}";
-    
+
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+
     int httpResponseCode = http.POST(jsonData);
 
     if (httpResponseCode > 0) {
       String response = http.getString();
+      Serial.println(httpResponseCode);
       Serial.println(response);
+      counter++;
     } else {
       Serial.print("Error on sending POST: ");
       Serial.println(httpResponseCode);
     }
+
     http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
   }
-  delay(100);
+
+  delay(1);
 }
